e9345f5ccfaf35be9ff99e03de28524575b42ca9
Aniruddh Patel <anni97708@gmail.com>
2025-09-29 18:56:07 +0530

    js mock questions2 done

---
+++ 29_09_2025(10).js
@@ -0,0 +1,60 @@
+// const students = [
+//   { name: "Aman", marks: { math: 90, english: 80, science: 85 } },
+//   { name: "Sneha", marks: { math: 70, english: 75, science: 80 } },
+//   { name: "Ravi", marks: { math: 95, english: 88, science: 92 } },
+// ];
+// [
+//   { name: "Aman", avg: 85, topSubject: "math", grade: "A" },
+//   { name: "Sneha", avg: 75, topSubject: "science", grade: "B" },
+//   { name: "Ravi", avg: 91.67, topSubject: "math", grade: "A" }
+// ]
+
+function main() {
+  const students = [
+    { name: "Aman", marks: { math: 90, english: 80, science: 85 } },
+    { name: "Sneha", marks: { math: 70, english: 75, science: 80 } },
+    { name: "Ravi", marks: { math: 95, english: 88, science: 92 } },
+  ];
+
+  const result = [];
+  students.forEach((data) => {
+    let avg = averagefunc(data.marks);
+    let {maxsub,grade} = topSubjectfunc(data.marks);
+    
+
+    result.push({ name: data.name, avg: avg, topSubject: maxsub, grade: grade });
+  });
+  console.log(result);
+}
+
+function averagefunc(marks) {
+    let sum=0;
+    for(let key in marks){
+        sum+=marks[key];
+    }
+    sum=sum/3
+    return sum.toFixed(2);
+}
+function topSubjectfunc(marks) {
+    let maxsub="";
+    let maxnum=0;
+    for(let key in marks){
+        if(marks[key]>maxnum){
+            maxnum=marks[key];
+            maxsub=key;
+        }
+    }
+    let grade = gradeCalfunc(maxnum);
+
+    return {maxsub,grade};
+}
+function gradeCalfunc(max_num) {
+    // (A = 85+, B = 70–84, C = below 70)
+    if(max_num<70)return "C";
+    else if(max_num>=70 && max_num<=84)return "B";
+    else if(max_num>=85) return "A"
+    else return "";
+}
+
+
+main()
